db9b2f0a195babb9ec0e68d8cc2f61f83f17c3a8
Boeing1337 <56965556+Boeing1337@users.noreply.github.com>
2020-05-28 23:10:11 +0300

    Stage 5 tests and refactoring

--- Chat/Stage 5/test/Tests.java
+++ Chat/Stage 5/test/Tests.java
@@ -1,9 +1,30 @@
-import org.junit.Test;
+import chat.client.Client;
+import chat.server.Server;
+import org.hyperskill.hstest.dynamic.input.DynamicTestingMethod;
+import org.hyperskill.hstest.stage.StageTest;
+import org.hyperskill.hstest.testcase.CheckResult;
+import org.hyperskill.hstest.testing.TestedProgram;
 
-import java.security.MessageDigest;
+public class Tests extends StageTest<String> {
 
-public class Tests {
-    @Test
-    public void testSolution() {
+    @DynamicTestingMethod
+    CheckResult test() {
+        final TestedProgram server = new TestedProgram(Server.class);
+        final TestedProgram client1 = new TestedProgram(Client.class);
+        final TestedProgram client2 = new TestedProgram(Client.class);
+        final TestedProgram client3 = new TestedProgram(Client.class);
+        client1.setReturnOutputAfterExecution(false);
+        client2.setReturnOutputAfterExecution(false);
+        client3.setReturnOutputAfterExecution(false);
+
+        server.startInBackground();
+        client1.start();
+        client2.start();
+
+        client1.execute("/auth asdasd asdasd");
+        System.out.println(client1.getOutput().trim());
+
+
+        return CheckResult.correct();
     }
 }
